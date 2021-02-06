7deb7fd83aa9d758588864105eaa716442a26527
Mahmudul Hasan <mahmudulh883@gmail.com>
2021-02-06 20:42:28 +0600

    multiline ,single line es6 templete features

---
+++ template.js
@@ -0,0 +1,7 @@
+const firstName='Mahmudul'
+const lastName='hasan'
+const fullName=firstName+' '+lastName + 'is a good boy'
+console.log(fullName)
+//multiline  Es6
+const fullName2=`${firstName} ${lastName} is a good boy who has ${100+900} taka`; 
+console.log(fullName2);
