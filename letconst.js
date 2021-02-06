e5b1b1c9e338469a5c0121043ddd82b18421023e
Mahmudul Hasan <mahmudulh883@gmail.com>
2021-02-06 19:56:28 +0600

    let const practice

---
+++ letconst.js
@@ -0,0 +1,27 @@
+//const hubby='hakim';
+//const hubby='saima';
+//console.log(hubby)
+const arr = [2,3,4,5];
+ arr[1] = 40;
+ arr.push(9);
+ arr.pop(3)
+console.log(arr);
+const nyok ={
+   name:'sakib khan',
+   phone:'0170',
+   name:'habib',
+   phone:'5030'}
+console.log(nyok);
+
+
+
+let patien ='rahima';
+    patien ='mahmud khan23';
+    console.log(patien );
+
+
+    let sum=0;
+    for (var x =0; x < 10; x++){    // var x =0; x < 10; x++ dile let leak kore baire console korle error show korbe
+      sum=sum+x; 
+      //console.log(x);
+         }   console.log(x);
