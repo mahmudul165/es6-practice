d23ea0ce09258ba8dfe4d84d35816d22890cf2e5
Mahmudul Hasan <mahmudulh883@gmail.com>
2021-02-06 21:57:09 +0600

    class in javascript

---
+++ class.js
@@ -0,0 +1,10 @@
+class student{
+   constructor(sId,sName){
+      this.id=sId;
+      this.name=sName;
+      this.school='rifles school & college'
+   }
+}
+const student1=new student(1,'mahmud');
+const student2=new student(1,'hasan');
+console.log(student1,student2);
